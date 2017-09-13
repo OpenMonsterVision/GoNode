51583bf7412022016f84fb5ab1ad6051a256869b
bsdpunk <bsdpunk@gmail.com>
2017-09-13 07:24:07 -0500

    theThings

--- jsFiles/jsTimesTables/dustyTimesThree.js
+++ jsFiles/jsTimesTables/dustyTimesThree.js
@@ -1,3 +1,4 @@
+#!/usr/local/bin/node
 const { spawn } = require('child_process');
 //const ls = spawn('ls', ['-lh', '/usr']);
 console.log(process.argv[2])
@@ -10,21 +11,22 @@ console.log(process.argv[2])
   //  timestable(n);
 //}
 
-if(process.argv[2] == false){
-
+if(process.argv[2] == undefined){
+        console.log("here")
     for(n = 2; n <= 12; n++ ){
-        const ls = spawn('node', ['dustyTimesThree.js', n, '>output']);
-        timetable(n)          
-ls.stdout.on('data', (data) => {
-      console.log(`stdout: ${data}`);
-});
+        var ls = spawn('node', ['dustyTimesThree.js', n]);
+        timestable(n)          
 
-ls.stderr.on('data', (data) => {
-      console.log(`stderr: ${data}`);
-});
-
-ls.on('close', (code) => {
-      console.log(`child process exited with code ${code}`);
-});
     }
 }
+//ls.stdout.on('data', (data) => {
+//      console.log(`stdout: ${data}`);
+//});
+//
+//ls.stderr.on('data', (data) => {
+//      console.log(`stderr: ${data}`);
+//});
+//
+//ls.on('close', (code) => {
+//      console.log(`child process exited with code ${code}`);
+//});
