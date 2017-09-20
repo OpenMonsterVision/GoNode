0ab6dbd8cd8c2de58513707bd8fbf051e0c3652c
bsdpunk <bsdpunk@gmail.com>
2017-09-20 14:29:00 -0500

    Format

--- jsFiles/jsRand/dustyRandTwo.js
+++ jsFiles/jsRand/dustyRandTwo.js
@@ -5,21 +5,21 @@ minone = parseInt(min) - 1
 maxone = parseInt(max) + 1
 //console.log(min + max)
 function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-    }
-    var a = [];
-	for(i = minone; i < max;){
-        b = getRandomInt(min,maxone) 
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+var a = [];
+for(i = minone; i < max;){
+    b = getRandomInt(min,maxone) 
 
-            if(!a[String(b)]){
-                a[String(b)] = i
-                    i++;
+        if(!a.includes(b)){
+            a[i] = b
+                i++;
         }
-        }
+}
 
 a = a.filter(function( element ) {
-       return element !== undefined;
+    return element !== undefined;
 });
 console.log(a)
