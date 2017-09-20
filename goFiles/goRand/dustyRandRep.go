6fd5fd2ec6a3d08b758bb9b5864445ca21acb922
bsdpunk <bsdpunk@gmail.com>
2017-09-20 13:16:48 -0500

    brokenRep

--- goFiles/goRand/dustyRandRep.go
+++ goFiles/goRand/dustyRandRep.go
@@ -4,14 +4,30 @@ import "time"
 import "fmt"
 import "math/rand"
 
+//import "unique"
+//import _ "github.com/KyleBanks/go-kit/unique"
+
+func unqInts(input []int) []int {
+	u := make([]int, 0, len(input))
+	m := make(map[int]bool)
+
+	for _, val := range input {
+		if _, ok := m[val]; !ok {
+			m[val] = true
+			u = append(u, val)
+		}
+	}
+
+	return u
+}
 func main() {
 
 	// For example, `rand.Intn` returns a random `int` n,
 	// `0 <= n < 100`.
-	fmt.Print(rand.Intn(17), ",")
-	fmt.Print(rand.Intn(17), ",")
-	fmt.Print(rand.Intn(17), ",")
-	fmt.Println(rand.Intn(17))
+	//fmt.Print(rand.Intn(17), ",")
+	//fmt.Print(rand.Intn(17), ",")
+	//fmt.Print(rand.Intn(17), ",")
+	//fmt.Println(rand.Intn(17))
 	//	fmt.Print(rand.Intn(100))
 	//fmt.Println()
 
@@ -24,11 +40,21 @@ func main() {
 	//fmt.Print((rand.Float64()*5)+5, ",")
 	s1 := rand.NewSource(time.Now().UnixNano())
 	r1 := rand.New(s1)
-
 	// Call the resulting `rand.Rand` just like the
 	// functions on the `rand` package.
 	fmt.Print(r1.Intn(17), ",")
+	fmt.Print(r1.Intn(17), ",")
+	fmt.Print(r1.Intn(17), ",")
 	fmt.Print(r1.Intn(17))
 	fmt.Println()
+	b := make([]int, 30)
+	for i := 0; i < 30; {
 
+		fmt.Print(r1.Intn(17), ",")
+		b[i] = r1.Intn(17)
+		i++
+
+	}
+	unique := unqInts(b)
+	println(unique)
 }
