0c78b2ba8491ee4c8e6b0dd1ef64c148f31eab40
bsdpunk <bsdpunk@gmail.com>
2017-09-20 14:31:24 -0500

    oopsOverWrote

--- jsFiles/jsRand/dustyRandTwo.js
+++ jsFiles/jsRand/dustyRandTwo.js
@@ -3,7 +3,7 @@ min = process.argv[2]
 max = process.argv[3]
 minone = parseInt(min) - 1
 maxone = parseInt(max) + 1
-//console.log(min + max)
+
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -13,8 +13,8 @@ var a = [];
 for(i = minone; i < max;){
     b = getRandomInt(min,maxone) 
 
-        if(!a.includes(b)){
-            a[i] = b
+        if(!a[String(b)]){
+            a[String(b)] = i
                 i++;
         }
 }
